0defb0b0c21ce3eebdf297b95509e5fcc8106107
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2018-10-04 13:26:04 +0200

    #5 Got basic async storage and retrieval to work

--- p3/pimm/Task.js
+++ p3/pimm/Task.js
@@ -1,27 +1,66 @@
-import React from 'react';
-import {Alert, AppRegistry, Platform, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View, Image } from 'react-native';
-
+import React from "react";
+import {
+  Alert,
+  AppRegistry,
+  AsyncStorage,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  TouchableWithoutFeedback,
+  View,
+  Image
+} from "react-native";
 
 export class Task extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state.taskdescription = props.taskdescription;
-    }
+  constructor(props) {
+    super(props);
+    this.state.taskdescription = props.taskdescription;
+  }
 
-    state = {
-        taskdescription: 'Hent melk'
+  state = {
+    taskdescription: "Hent melk"
+  };
+
+  componentDidMount() {
+    _storeData = async () => {
+      try {
+        await AsyncStorage.setItem("Task1", "Hallo der du!");
+      } catch (error) {
+        Alert.alert("Error saving data!");
+      }
     };
 
-    _onPressButton = () => {
-      Alert.alert("yeleo");
-    }
+    _retrieveData = async () => {
+      try {
+        const value = await AsyncStorage.getItem("Task1");
+        if (value != null) {
+          console.log(value);
+        }
+      } catch (error) {
+        Alert.alert("Error retrieving data");
+      }
+    };
 
-    render() {
+    _storeData();
+    _retrieveData();
+  }
+
+  _onPressButton = () => {
+    Alert.alert("yeleo");
+  };
+
+  render() {
     return (
       <View style={styles.task}>
         <Text style={styles.taskdescription}>{this.state.taskdescription}</Text>
         <TouchableOpacity onPress={this._onPressButton}>
-          <Image source={require("./assets/acceptbutton.png")} style={styles.acceptbutton}/>
+          <Image
+            source={require("./assets/acceptbutton.png")}
+            style={styles.acceptbutton}
+          />
         </TouchableOpacity>
       </View>
     );
@@ -30,13 +69,13 @@ export class Task extends React.Component {
 
 const styles = StyleSheet.create({
   task: {
-    flexDirection: 'row',
-    backgroundColor: '#eee',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
+    flexDirection: "row",
+    backgroundColor: "#eee",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
     height: 100,
-    borderColor: '#999',
+    borderColor: "#999",
     borderWidth: 3,
     borderRadius: 5,
     paddingLeft: 10,
@@ -44,12 +83,12 @@ const styles = StyleSheet.create({
   },
 
   acceptbutton: {
-     width: 48,
-     height: 48
+    width: 48,
+    height: 48
   },
 
   taskdescription: {
-    color: '#333',
+    color: "#333",
     flex: 5
   }
 });
