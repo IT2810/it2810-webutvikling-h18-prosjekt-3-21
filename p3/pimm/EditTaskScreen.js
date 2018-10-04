5b32786cd8a43e8a7b89178b21ec2a66bd82e3e6
Simen Kjernlie <simen.kjernlie@gmail.com>
2018-10-04 13:41:13 +0200

    Updated edit task screen

--- p3/pimm/EditTaskScreen.js
+++ p3/pimm/EditTaskScreen.js
@@ -1,23 +1,53 @@
 import React, { Component } from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+  Picker
+} from "react-native";
 import { styles } from "./HomeScreen";
 
 export class EditTaskScreen extends Component {
   constructor(props) {
     super(props);
     this.state.parent = props.parent;
+    this.state.taskDescription = "";
   }
   state = {};
   render() {
     return (
       <View style={styles.container}>
-        <View style={{ flex: 3 }}>
-          <Text style={(styles.headerText, { marginTop: 40 })}> Edit task</Text>
+        <View style={styles.header}>
+          <TouchableOpacity style={styles.button}>
+            <View>
+              <Text style={styles.buttonText}>Add task</Text>
+            </View>
+          </TouchableOpacity>
         </View>
-        <View style={{ flex: 1 }}>
+        <View style={styles.mainContainer}>
+          <TextInput
+            style={stylesEditScreen.textInputField}
+            placeholder="Add task description here"
+            blurOnSubmit={true}
+            multiline={true}
+            placeholderTextColor={"gray"}
+            onChangeText={text => this.setState({ taskDescriptions: text })}
+          />
+        </View>
+        <View style={styles.navBarBottom}>
           <TouchableOpacity onPress={this.onPressBack} style={styles.button}>
             <View>
-              <Text style={styles.buttonText}>Back to home screen</Text>
+              <Text style={styles.buttonText}>Back</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={this.onPressSaveTask}
+            style={styles.button}
+          >
+            <View>
+              <Text style={styles.buttonText}>Save task</Text>
             </View>
           </TouchableOpacity>
         </View>
@@ -29,3 +59,10 @@ export class EditTaskScreen extends Component {
     this.state.parent.handlePressBack();
   };
 }
+
+export const stylesEditScreen = StyleSheet.create({
+  textInputField: {
+    height: 60,
+    margin: 10
+  }
+});
