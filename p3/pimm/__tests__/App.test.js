51117e6f016fe7ac2e550c709fbe0596a0fd2449
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2018-10-17 11:15:53 +0200

    #23 Wrote about AsyncStorage and resolved a conflict in App.test.js

--- p3/pimm/__tests__/App.test.js
+++ p3/pimm/__tests__/App.test.js
@@ -5,9 +5,6 @@ import renderer from "react-test-renderer";
 
 test("renders correctly", () => {
   const tree = renderer.create(<App />).toJSON();
-<<<<<<< HEAD
-=======
   expect(tree.type).toEqual("View");
->>>>>>> master
   expect(tree).toMatchSnapshot();
 });
